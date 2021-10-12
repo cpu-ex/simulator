4a7b09fbcc9b5be31b58e56785d8bd07e5e327cb
PENG AO <pengao1293@gmail.com>
2021-10-12 14:26:49 +0900

    improve the definition of basic types

--- types.h
+++ types.h
@@ -3,6 +3,10 @@
 
 #define sext(val, shift) (val) | (((val) & (1 << (shift))) ? ~((1 << (shift)) - 1) : 0)
 
+typedef unsigned char u_int8_t;
+typedef unsigned short u_int16_t;
+typedef unsigned int u_int32_t;
+
 typedef u_int8_t  BYTE;
 typedef u_int16_t HALF;
 typedef u_int32_t WORD;
