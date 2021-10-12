4f443d6e9707feb2a2028c188b4a6b00f5af5431
PENG AO <pengao1293@gmail.com>
2021-10-12 16:52:58 +0900

    optimize the GUI slightly

--- win.c
+++ win.c
@@ -10,40 +10,80 @@
 
 static WIN* win_base;
 
-void show_help_info() {
-    WINDOW* win;
-
-    win = win_base->mem_win;
-    wclear(win);
-    mvwprintw(win, 1, 3, "step [n]:\n\tmove on for n step, default to 1");
-    mvwprintw(win, 3, 3, "reg [d/a/s/t]:\n\tswitch register set, default to zero -> a5");
-    mvwprintw(win, 5, 3, "reg [-][reg name]:\n\thighlight certain register,\n\tminus for setting back to normal,\n\tmutiple input supported");
-    mvwprintw(win, 9, 3, "mem [address]:\n\tswitch memory range, default to 0x0");
-    mvwprintw(win, 11, 3, "help:\n\tshow help window");
-    mvwprintw(win, 13, 3, "quit:\n\texit simulator");
-
-    win = win_base->com_win;
-    wclear(win);
-    char info[] = "-> press any key to skip <-";
-    wattron(win, COLOR_PAIR(STANDOUT_COLOR));
-    mvwprintw(win, 0, (78 - strlen(info)) / 2, info);
-    wattroff(win, COLOR_PAIR(STANDOUT_COLOR));
-
-    refresh();
-    wrefresh(win_base->mem_win);
-    wgetch(win_base->com_win);
+void show_main_win() {
+    // create outer windows (containers that never change)
+    WINDOW* pc_outer = newwin(3, 80, 0, 0);
+    WINDOW* reg_outer = newwin(18, 14, 3, 0);
+    WINDOW* mem_outer = newwin(18, 66, 3, 14);
+    WINDOW* com_outer = newwin(3, 80, 21, 0);
+    // box the containers
+    wattron(pc_outer, COLOR_PAIR(TITLE_COLOR));
+    wattron(reg_outer, COLOR_PAIR(TITLE_COLOR));
+    wattron(mem_outer, COLOR_PAIR(TITLE_COLOR));
+    wattron(com_outer, COLOR_PAIR(TITLE_COLOR));
+    box(pc_outer, 0, 0); mvwprintw(pc_outer, 0, 2, " PC "); wrefresh(pc_outer);
+    box(reg_outer, 0, 0); mvwprintw(reg_outer, 0, 2, " Register "); wrefresh(reg_outer);
+    box(mem_outer, 0, 0); mvwprintw(mem_outer, 0, 2, " Memory "); wrefresh(mem_outer);
+    box(com_outer, 0, 0); mvwprintw(com_outer, 0, 2, " Input "); wrefresh(com_outer);
+    wattroff(pc_outer, COLOR_PAIR(TITLE_COLOR));
+    wattroff(reg_outer, COLOR_PAIR(TITLE_COLOR));
+    wattron(mem_outer, COLOR_PAIR(TITLE_COLOR));
+    wattron(com_outer, COLOR_PAIR(TITLE_COLOR));
+    // create inner windows (contents that actually display unpdates)
+    if (!win_base->pc_win) {
+        static WINDOW* pc_inner; pc_inner = newwin(1, 78, 1, 1);
+        win_base->pc_win = pc_inner;
+    }
+    if (!win_base->reg_win) {
+        static WINDOW* reg_inner; reg_inner = newwin(16, 12, 4, 1);
+        win_base->reg_win = reg_inner;
+    }
+    if (!win_base->mem_win) {
+        static WINDOW* mem_inner; mem_inner = newwin(16, 64, 4, 15);
+        win_base->mem_win = mem_inner;
+    }
+    if (!win_base->com_win) {
+        static WINDOW* com_inner; com_inner = newwin(1, 77, 22, 2);
+        win_base->com_win = com_inner;
+    }
 }
 
-void show_splash_info() {
-    // some simple instructions of using simulator
-    WINDOW* win = win_base->com_win;
-    char info[] = "-> press any key but 'h' to skip <-";
-    wattron(win, COLOR_PAIR(STANDOUT_COLOR));
-    mvwprintw(win, 0, (78 - strlen(info)) / 2, info);
-    wattroff(win, COLOR_PAIR(STANDOUT_COLOR));
+void show_help_win() {
+    clear();
+    WINDOW* help_win_outer = newwin(16, 70, 4, 5);
+    WINDOW* help_win_inner = newwin(14, 68, 5, 6);
+    box(help_win_outer, 0, 0);
+    mvwprintw(help_win_outer, 0, 2, "Instruction");
+    mvwprintw(help_win_inner, 0, 3, "step [n]:\n\tmove on for n step, default to 1");
+    mvwprintw(help_win_inner, 2, 3, "reg [d/a/s/t]:\n\tswitch register set, default to zero -> a5");
+    mvwprintw(help_win_inner, 4, 3, "reg [-][reg name]:\n\thighlight certain register,\n\tminus for setting back to normal,\n\tmutiple input supported");
+    mvwprintw(help_win_inner, 8, 3, "mem [address]:\n\tswitch memory range, default to 0x0");
+    mvwprintw(help_win_inner, 10, 3, "help:\n\tshow help window");
+    mvwprintw(help_win_inner, 12, 3, "quit:\n\texit simulator");
     refresh();
-    if (wgetch(win) == 'h')
-        show_help_info();
+    wrefresh(help_win_outer);
+    wgetch(help_win_inner);
+    free(help_win_outer);
+    free(help_win_inner);
+}
+
+void show_splash_win() {
+    WINDOW* splash_box = newwin(6, 40, 6, 20);
+    box(splash_box, 0, 0);
+    char title[] = "RISC-V simulator";
+    char author[] = "PENG AO";
+    char info[] = "-> press any key but 'h' to skip <-";
+    mvwprintw(splash_box, 2, (40 - strlen(title)) / 2, title);
+    mvwprintw(splash_box, 3, (40 - strlen(author)) / 2, author);
+    mvprintw(13, (80 - strlen(info)) / 2, info);
+    refresh();
+    wrefresh(splash_box);
+    // catch char h
+    if (getch() == 'h') {
+        show_help_win();
+    }
+    free(splash_box);
+    show_main_win();
 }
 
 typedef struct command {
@@ -160,7 +200,7 @@ STATE wait4command() {
         win_base->mem_focus = com.argc ? com.argv[0] : 0xFFFFFFFF;
         return STAT_HALT;
     case 'h':
-        show_help_info();
+        show_help_win();
     default:
         return STAT_HALT;
     }
@@ -219,6 +259,7 @@ void update_com(CORE* core) {
 }
 
 STATE update(CORE* core) {
+    show_main_win();
     update_pc(core);
     update_reg(core);
     update_mem(core);
@@ -254,41 +295,7 @@ void init_win(WIN* win) {
     init_pair(SUBTITLE_COLOR, COLOR_CYAN, COLOR_BLACK);
     init_pair(STANDOUT_COLOR, COLOR_RED, COLOR_BLACK);
     init_pair(FOCUS_COLOR, COLOR_MAGENTA, COLOR_BLACK);
-    // create outer windows (containers that never change)
-    WINDOW* pc_outer = newwin(3, 78 - strlen(title), 0, 2 + strlen(title));
-    WINDOW* reg_outer = newwin(18, 14, 3, 0);
-    WINDOW* mem_outer = newwin(18, 66, 3, 14);
-    WINDOW* com_outer = newwin(3, 80, 21, 0);
-    refresh();
-    // box the containers and name the titles
-    wattron(pc_outer, COLOR_PAIR(TITLE_COLOR));
-    wattron(reg_outer, COLOR_PAIR(TITLE_COLOR));
-    wattron(mem_outer, COLOR_PAIR(TITLE_COLOR));
-    wattron(com_outer, COLOR_PAIR(TITLE_COLOR));
-    box(pc_outer, 0, 0); mvwprintw(pc_outer, 0, 2, " PC "); wrefresh(pc_outer);
-    box(reg_outer, 0, 0); mvwprintw(reg_outer, 0, 2, " Register "); wrefresh(reg_outer);
-    box(mem_outer, 0, 0); mvwprintw(mem_outer, 0, 2, " Memory "); wrefresh(mem_outer);
-    box(com_outer, 0, 0); mvwprintw(com_outer, 0, 2, " Input "); wrefresh(com_outer);
-    wattroff(pc_outer, COLOR_PAIR(TITLE_COLOR));
-    wattroff(reg_outer, COLOR_PAIR(TITLE_COLOR));
-    wattron(mem_outer, COLOR_PAIR(TITLE_COLOR));
-    wattron(com_outer, COLOR_PAIR(TITLE_COLOR));
-    // create inner windows (contents that actually display unpdates)
-    static WINDOW* pc_inner; pc_inner = newwin(1, 76 - strlen(title), 1, 3 + strlen(title));
-    static WINDOW* reg_inner; reg_inner = newwin(16, 12, 4, 1);
-    static WINDOW* mem_inner; mem_inner = newwin(16, 64, 4, 15);
-    static WINDOW* com_inner; com_inner = newwin(1, 77, 22, 2);
-    // show basic info
-    attron(A_STANDOUT);
-    mvprintw(1, 1, title);
-    attroff(A_STANDOUT);
-    mvprintw(23, 72, " PENG AO ");
-    // register inner windows to WIN
-    win->pc_win = pc_inner;
-    win->reg_win = reg_inner;
-    win->mem_win = mem_inner;
-    win->com_win = com_inner;
-
+    // regist variables
     win->reg_set = REG_SET_DEF;
     memset(win->reg_focus, 0, 32);
     win->mem_start = 0x0;
@@ -297,5 +304,5 @@ void init_win(WIN* win) {
     win->update = update;
     win->deinit = deinit;
 
-    show_splash_info();
+    show_splash_win();
 }
